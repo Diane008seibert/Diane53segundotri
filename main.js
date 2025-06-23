7155cea576f4f6109e807188fee93ba6a6d051a3
Diane008seibert <diane.seibert@escola.pr.gov.br>
2025-06-23 18:55:01 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-geraSenha();
+numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -9,6 +9,7 @@ if (tamanhoSenha > 1){
 /*tamanhoSenha = tamanhoSenha-1;*/
 tamanhoSenha--;
 }
+numeroSenha.textContent = tamanhoSenha;
 geraSenha();
 }
 function aumentaTamanho(){
@@ -16,6 +17,7 @@ if (tamanhoSenha < 20){
 /*tamanhoSenha = tamanhoSenha+1;*/
 tamanhoSenha++;
 }
+numeroSenha.textContent = tamanhoSenha;
 geraSenha();
 }
 const campoSenha = document.querySelector('#campo-senha');
@@ -23,6 +25,7 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 geraSenha();
 function geraSenha() {
 let senha = '';
+
 for (let i = 0; i < tamanhoSenha; i++) {
 let numeroAleatorio = Math.random() * letrasMaiusculas.length;
 numeroAleatorio = Math.floor(numeroAleatorio);
